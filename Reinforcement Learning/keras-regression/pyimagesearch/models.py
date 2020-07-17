f85491bcbbb7fa8a62e0b7a6856d9810ace4b425
Armand <armandsarkani@icloud.com>
2020-07-16 17:16:00 -0700

    Added keras tutorial

---
+++ Reinforcement Learning/keras-regression/pyimagesearch/models.py
@@ -0,0 +1,24 @@
+# import the necessary packages
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+
+def create_mlp(dim, regress=False):
+	# define our MLP network
+	model = Sequential()
+	model.add(Dense(8, input_dim=dim, activation="relu"))
+	model.add(Dense(4, activation="relu"))
+
+	# check to see if the regression node should be added
+	if regress:
+		model.add(Dense(1, activation="linear"))
+
+	# return our model
+	return model
